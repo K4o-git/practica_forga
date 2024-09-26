7c01c96b211230ebf7aa3474585efbe8d6a4fdf7
K4o-git <nachogb.op@gmail.com>
2024-09-26 13:55:14 +0200

    1º interfaz con tksheet

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import csv
 import os
 import sys
+import io
 from pathlib import Path
 
 import pandas as pd
@@ -9,9 +11,8 @@ import win32api
 import win32print
 import tkinter as tk
 from tkinter import *
-from tkinter import ttk
+from tkinter import ttk, filedialog
 from tksheet import Sheet
-
 
 class win(tk.Tk):
     def __init__(self):
@@ -24,18 +25,25 @@ class win(tk.Tk):
         self.frame.grid_columnconfigure(0, weight=1)
         self.frame.grid_rowconfigure(0, weight=1)
         self.sheet = tksheet.Sheet(self, width=980, height=450, total_columns=4, total_rows=15 , show_x_scrollbar=False, show_y_scrollbar=True)
-        self.sheet.change_theme("dark")
         self.sheet.column_width(column=0, width=140)
         self.sheet.column_width(column=1, width=380)
         self.sheet.column_width(column=2, width=380)
         self.sheet.column_width(column=3, width=24)
-        self.sheet.headers(["DNI", "Nome", "Apelidos", "✅"])
+        self.sheet.headers(["DNI", "Nome", "Apelidos", "ok"])
         self.sheet.checkbox("D", checked=False)
         self.sheet.enable_bindings()
-        self.sheet.disable_bindings("column_width_resize")
+        self.sheet.disable_bindings("column_width_resize", "rc_insert_column", "rc_delete_column")
+        self.sheet.popup_menu_add_command("Cargar archivo csv", self.open_csv)
+        self.sheet.popup_menu_add_command("Guardar datos a CSV", self.save_sheet)
         self.frame.grid(row=1, column=0, sticky="nswe")
         self.sheet.grid(row=1, column=0, sticky="nswe")
 
+        self.sheet_span = self.sheet.span(
+            header=True,
+            index=False,
+            hdisp=False,
+            idisp=False,
+        )
 
         #CAMPOS DE TEXTO PARA LOS DATOS DEL CURSO
         self.frame2 = tk.Frame(self)
@@ -44,23 +52,23 @@ class win(tk.Tk):
 
         et_num_curso = Label(self.frame2, text="Nº do curso")
         et_num_curso.grid(row=0, column=0, padx=10, pady=10)
-        num_curso  = Entry(self.frame2, width=42)
-        num_curso.grid(row=0, column=1, padx=10, pady=10)
+        self.num_curso  = Entry(self.frame2, width=42)
+        self.num_curso.grid(row=0, column=1, padx=10, pady=10)
 
         et_nom_curso = Label(self.frame2, text="Nome do curso")
         et_nom_curso.grid(row=1, column=0, padx=10, pady=10)
-        nom_curso = Entry(self.frame2, width=42)
-        nom_curso.grid(row=1, column=1, padx=10, pady=10)
+        self.nom_curso = Entry(self.frame2, width=42)
+        self.nom_curso.grid(row=1, column=1, padx=10, pady=10)
 
         et_censo = Label(self.frame2, text="Nº Censo")
         et_censo.grid(row=2, column=0, padx=10, pady=10)
-        censo = Entry(self.frame2, width=42)
-        censo.grid(row=2, column=1, padx=10, pady=10)
+        self.censo = Entry(self.frame2, width=42)
+        self.censo.grid(row=2, column=1, padx=10, pady=10)
 
         et_centro = Label(self.frame2, text="Centro")
         et_centro.grid(row=3, column=0, padx=10, pady=10)
-        centro = Entry(self.frame2, width=42)
-        centro.grid(row=3, column=1, padx=10, pady=10)
+        self.centro = Entry(self.frame2, width=42)
+        self.centro.grid(row=3, column=1, padx=10, pady=10)
 
         titulo_tabla = Label(self.frame2, text="LISTADO DE ALUMNOS", font=('bold', 12, 'underline')).grid(row=4, column=3)
 
@@ -78,51 +86,90 @@ class win(tk.Tk):
         self.checkbox7 = ttk.Checkbutton(self.frame3, text="Modelo autorización datos persoais 2").grid(row=6, sticky="w")
         self.checkbox8 = ttk.Checkbutton(self.frame3, text="Modelo autorización rexistro pegada dixital_gal").grid(row=7, sticky="w")
 
-        botonOK = Button(self, width=20, text="LISTO", command=lambda: print('aaa')).grid(row=2, column=0, sticky="n", rowspan=2)
+        botonOK = Button(self, width=20, text="LISTO", command=lambda: self.prueba_generar_documentos()).grid(row=2, column=0, sticky="n", rowspan=2)
 
         barra_menus = Menu()
         menu = Menu(barra_menus, tearoff=False)
+        menu2 = Menu(barra_menus, tearoff=False)
         barra_menus.add_cascade(menu=menu, label="Archivo")
-        menu.add_command(label="Guardar datos", command=lambda: print('nada'))
-        menu.add_command(label="Cargar datos guardados", command=lambda: print('nada'))
+        barra_menus.add_cascade(menu=menu2, label="Opciones")
+        menu.add_command(label="Guardar datos", command=lambda: self.open_csv())
+        menu.add_command(label="Cargar datos guardados", command=lambda: self.save_sheet())
+
         self.config(menu=barra_menus)
 
-        #print(self.sheet.get_data())
+    def save_sheet(self):
+        filepath = filedialog.asksaveasfilename(
+            parent=self,
+            title="Save sheet as",
+            filetypes=[("CSV File", ".csv"), ("TSV File", ".tsv")],
+            defaultextension=".csv",
+            confirmoverwrite=True,
+        )
+        if not filepath or not filepath.lower().endswith((".csv", ".tsv")):
+            return
+        try:
+            file = open(filepath, 'w+')
+            file.write("DNI;Nome;Apelidos\n")
+            for row in self.sheet.data:
+                if (row[0] == ""): break
+                file.write(row[0] + ";" + row[1] + ";" + row[2] + "\n")
+        except FileNotFoundError:
+            print('ERROR')
+
+    def open_csv(self):
+        filepath = filedialog.askopenfilename(parent=self, title="Cargar archivo CSV")
+        if not filepath or not filepath.lower().endswith((".csv", ".tsv")):
+            return
+        try:
+            with open(Path(filepath), "r") as filehandle:
+                filedata = filehandle.read()
+            self.sheet.data.clear()
+            self.sheet_span.data = [
+                r
+                for r in csv.reader(
+                        io.StringIO(filedata),
+                    dialect=csv.Sniffer().sniff(filedata),
+                    skipinitialspace=False,
+                )
+            ]
+        except Exception as error:
+            print(error)
+            return
+
+    def prueba_generar_documentos(self):
+        for row in self.sheet.data:
+            if (row[0] == ""): break
+            nome = row[0]
+            apelidos = row[1]
+            dni = row[2]
+            id_curso = self.num_curso.get()
+            nome_curso = self.nom_curso.get()
+            centro = ''
+            censo = self.censo.get()
+
+            context = {
+                "DNI": dni,
+                "NOME": nome,
+                "APELIDOS": apelidos,
+                "ID_CURSO": id_curso,
+                "NOME_CURSO": nome_curso,
+                "CENTRO": centro,
+                "CENSO": censo
+            }
+            # Crea las subcarpetas para cada alumno y cuarda dentro sus documentos modificados
+            os.makedirs(str("generados/" + apelidos + " " + nome))
+            for file in os.listdir("./plantillas"):
+                if file.endswith("docx"):
+                    documento_path = Path(__file__).parent / str("plantillas/" + file)
+                    doc = DocxTemplate(documento_path)
+                    doc.render(context)
+                    doc.save(Path(__file__).parent / str("generados/" + apelidos + " " + nome + "/" + file))
+                    aaa = open(Path(__file__).parent / str("generados/" + apelidos + " " + nome + "/" + file), encoding="utf-8")
+                    print(aaa.name)
+                    #win32api.ShellExecute(0, 'print', aaa.name, f'/d:"{win32print.GetDefaultPrinter()}"', '.', 0)
+                else:
+                    print("fallo")
 
 app = win()
 app.mainloop()
-
-def prueba_generar_documentos():
-    archivo_alumnos = open('alumnos.csv', encoding="utf-8")
-    for linea in archivo_alumnos:
-        if (linea.split(";")[0].strip() == 'Nome'): continue
-        nome = linea.split(";")[0].strip()
-        apelidos = linea.split(";")[1].strip()
-        dni = linea.split(";")[2].strip()
-        id_curso = ''
-        nome_curso = ''
-        data_alta = ''
-        censo = ''
-
-        context = {
-            "DNI": dni,
-            "NOME": nome,
-            "APELIDOS": apelidos,
-            "ID_CURSO": id_curso,
-            "NOME_CURSO": nome_curso,
-            "DATA_ALTA": data_alta,
-            "CENSO": censo
-        }
-        # Crea las subcarpetas para cada alumno y cuarda dentro sus documentos modificados
-        os.makedirs(str("generados/" + apelidos + " " + nome))
-        for file in os.listdir("./plantillas"):
-            if file.endswith("docx"):
-                documento_path = Path(__file__).parent / str("plantillas/" + file)
-                doc = DocxTemplate(documento_path)
-                doc.render(context)
-                doc.save(Path(__file__).parent / str("generados/" + apelidos + " " + nome + "/" + file))
-                aaa = open(Path(__file__).parent / str("generados/" + apelidos + " " + nome + "/" + file), encoding="utf-8")
-                print(aaa.name)
-                #win32api.ShellExecute(0, 'print', aaa.name, f'/d:"{win32print.GetDefaultPrinter()}"', '.', 0)
-            else:
-                print("fallo")
