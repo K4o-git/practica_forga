4932f6192a9da767eff1ce22c1cbf83c7843f9e1
K4o-git <nachogb.op@gmail.com>
2024-09-27 22:55:19 +0200

    Arreglada la tabla, casi todo funcional.

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ class win(tk.Tk):
         self.frame = tk.Frame(self)
         self.frame.grid_columnconfigure(0, weight=1)
         self.frame.grid_rowconfigure(0, weight=1)
-        self.sheet = tksheet.Sheet(self, width=980, height=450, total_columns=4, total_rows=15 , show_x_scrollbar=False, show_y_scrollbar=True)
+        self.sheet = tksheet.Sheet(self, width=980, height=450, total_columns=4, total_rows=15 , show_x_scrollbar=True, show_y_scrollbar=True)
         self.sheet.column_width(column=0, width=140)
         self.sheet.column_width(column=1, width=380)
         self.sheet.column_width(column=2, width=380)
@@ -72,32 +72,32 @@ class win(tk.Tk):
 
         titulo_tabla = Label(self.frame2, text="LISTADO DE ALUMNOS", font=('bold', 12, 'underline')).grid(row=4, column=3)
 
-
         #BOTONES DE SELECCIÓN DE DOCUMENTOS
         self.frame3 = tk.Frame(self.frame2)
         self.frame3.grid(row=0, column=4, padx=10, sticky="nswe", rowspan=4)
 
-        self.checkbox1 = ttk.Checkbutton(self.frame3, text="Ficha de alumno").grid(row=0, sticky="w")
-        self.checkbox2 = ttk.Checkbutton(self.frame3, text="Dereitos e deberes").grid(row=1, sticky="w")
-        self.checkbox3 = ttk.Checkbutton(self.frame3, text="Protección de datos").grid(row=2, sticky="w")
-        self.checkbox4 = ttk.Checkbutton(self.frame3, text="Rexistro pegada dixital").grid(row=3, sticky="w")
-        self.checkbox5 = ttk.Checkbutton(self.frame3, text="Información bolsas").grid(row=4, sticky="w")
-        self.checkbox6 = ttk.Checkbutton(self.frame3, text="Modelo autorización datos persoais").grid(row=5, sticky="w")
-        self.checkbox7 = ttk.Checkbutton(self.frame3, text="Modelo autorización datos persoais 2").grid(row=6, sticky="w")
-        self.checkbox8 = ttk.Checkbutton(self.frame3, text="Modelo autorización rexistro pegada dixital_gal").grid(row=7, sticky="w")
+        self.check1 = ttk.Checkbutton(self.frame3, text="Ficha de alumno").grid(row=0, sticky="w")
+        self.check2 = ttk.Checkbutton(self.frame3, text="Dereitos e deberes").grid(row=1, sticky="w")
+        self.check3 = ttk.Checkbutton(self.frame3, text="Protección de datos").grid(row=2, sticky="w")
+        self.check4 = ttk.Checkbutton(self.frame3, text="Rexistro pegada dixital").grid(row=3, sticky="w")
+        self.check5 = ttk.Checkbutton(self.frame3, text="Información bolsas").grid(row=4, sticky="w")
+        self.check6 = ttk.Checkbutton(self.frame3, text="Modelo autorización datos persoais").grid(row=5, sticky="w")
+        self.check7 = ttk.Checkbutton(self.frame3, text="Modelo autorización datos persoais 2").grid(row=6, sticky="w")
+        self.check8 = ttk.Checkbutton(self.frame3, text="Modelo autorización rexistro pegada dixital_gal").grid(row=7, sticky="w")
 
-        botonOK = Button(self, width=20, text="LISTO", command=lambda: self.prueba_generar_documentos()).grid(row=2, column=0, sticky="n", rowspan=2)
+        botonOK = Button(self, width=20, text="LISTO", command=lambda: self.prueba()).grid(row=2, column=0, sticky="n", rowspan=2)
 
         barra_menus = Menu()
         menu = Menu(barra_menus, tearoff=False)
         menu2 = Menu(barra_menus, tearoff=False)
         barra_menus.add_cascade(menu=menu, label="Archivo")
         barra_menus.add_cascade(menu=menu2, label="Opciones")
-        menu.add_command(label="Guardar datos", command=lambda: self.open_csv())
-        menu.add_command(label="Cargar datos guardados", command=lambda: self.save_sheet())
-
+        menu.add_command(label="Guardar datos", command=lambda: self.save_sheet())
+        menu.add_command(label="Cargar datos guardados", command=lambda: self.open_csv())
+        menu.add_command(label="Salir", command=lambda: sys.exit())
         self.config(menu=barra_menus)
 
+    #Guarda los datos de la tabla en el estado actual, excepto los valores de la tabla de checkbox.
     def save_sheet(self):
         filepath = filedialog.asksaveasfilename(
             parent=self,
@@ -110,66 +110,84 @@ class win(tk.Tk):
             return
         try:
             file = open(filepath, 'w+')
-            file.write("DNI;Nome;Apelidos\n")
+            #file.write("DNI;Nome;Apelidos;ok\n")
             for row in self.sheet.data:
-                if (row[0] == ""): break
-                file.write(row[0] + ";" + row[1] + ";" + row[2] + "\n")
+                check = ''
+                if (row[3]): check = str(row[3])
+                file.write(row[0] + ";" + row[1] + ";" + row[2] + ";" + check + "\n")
         except FileNotFoundError:
             print('ERROR')
 
+    #Carga los datos en la tabla. Ya no da error al seleccionar o deseleccionar en la columna de checkbox
     def open_csv(self):
         filepath = filedialog.askopenfilename(parent=self, title="Cargar archivo CSV")
         if not filepath or not filepath.lower().endswith((".csv", ".tsv")):
             return
         try:
-            with open(Path(filepath), "r") as filehandle:
-                filedata = filehandle.read()
-            self.sheet.data.clear()
-            self.sheet_span.data = [
-                r
-                for r in csv.reader(
-                        io.StringIO(filedata),
-                    dialect=csv.Sniffer().sniff(filedata),
-                    skipinitialspace=False,
-                )
-            ]
+            with open(Path(filepath), "r") as file:
+                self.sheet.data.clear()
+                i = 1
+                for linea in file:
+                    dni = linea.split(';')[0]
+                    nome = linea.split(';')[1]
+                    apelidos = linea.split(';')[2]
+                    self.sheet["A" + str(i)].data = [dni, nome, apelidos, False]
+                    i += 1
         except Exception as error:
             print(error)
             return
 
+    #Función para rellenar los ducumentos a partir de los datos de la tabla y los campos de texto. Por ahora no va con selección, se generan todos.
     def prueba_generar_documentos(self):
         for row in self.sheet.data:
-            if (row[0] == ""): break
-            nome = row[0]
-            apelidos = row[1]
-            dni = row[2]
-            id_curso = self.num_curso.get()
-            nome_curso = self.nom_curso.get()
-            centro = ''
-            censo = self.censo.get()
+            try:
+                if (row[0] == "") or (row[3] == None): break
+                nome = row[0]
+                apelidos = row[1]
+                dni = row[2]
+                id_curso = self.num_curso.get()
+                nome_curso = self.nom_curso.get()
+                centro = ''
+                censo = self.censo.get()
 
-            context = {
-                "DNI": dni,
-                "NOME": nome,
-                "APELIDOS": apelidos,
-                "ID_CURSO": id_curso,
-                "NOME_CURSO": nome_curso,
-                "CENTRO": centro,
-                "CENSO": censo
+                context = {
+                    "DNI": dni,
+                    "NOME": nome,
+                    "APELIDOS": apelidos,
+                    "ID_CURSO": id_curso,
+                    "NOME_CURSO": nome_curso,
+                    "CENTRO": centro,
+                    "CENSO": censo
+                }
+                # Crea las subcarpetas para cada alumno y cuarda dentro sus documentos modificados
+                os.makedirs(str("generados/" + apelidos + " " + nome))
+                for file in os.listdir("./plantillas"):
+                    if file.endswith("docx"):
+                        documento_path = Path(__file__).parent / str("plantillas/" + file)
+                        doc = DocxTemplate(documento_path)
+                        doc.render(context)
+                        doc.save(Path(__file__).parent / str("generados/" + apelidos + " " + nome + "/" + file))
+                        archivo = open(Path(__file__).parent / str("generados/" + apelidos + " " + nome + "/" + file), encoding="utf-8")
+                        print(archivo.name)
+                        #win32api.ShellExecute(0, 'print', archivo.name, f'/d:"{win32print.GetDefaultPrinter()}"', '.', 0)
+                    else:
+                        print("fallo")
+            except Exception as e:
+                print(e)
+
+    #Èsto por ahora no se usa, es parte de una idea para que se impriman sólo los seleccionados con los checkbox.
+    plantillas = {1:"./plantillas/Plantilla_Ficha_alumn_AFD.docx",
+            2:"./plantillas/Plantilla_dereitos-deberes_2.docx",
+            3:"./plantillas/Plantilla_proteccion-datos.docx",
+            4:"./plantillas/Plantilla_rexistro-pegada_2.docx",
+            5:"./plantillas/Plantilla_Informacion-Bolsas.docx",
+            6:"./plantillas/Plantilla_Modelo autorización datos persoais.docx",
+            7:"./plantillas/Plantilla_Modelo autorización datos persoais_2.docx",
+            8:"./plantillas/Plantilla_Modelo autorización rexistro pegada dixital_gal.docx"
             }
-            # Crea las subcarpetas para cada alumno y cuarda dentro sus documentos modificados
-            os.makedirs(str("generados/" + apelidos + " " + nome))
-            for file in os.listdir("./plantillas"):
-                if file.endswith("docx"):
-                    documento_path = Path(__file__).parent / str("plantillas/" + file)
-                    doc = DocxTemplate(documento_path)
-                    doc.render(context)
-                    doc.save(Path(__file__).parent / str("generados/" + apelidos + " " + nome + "/" + file))
-                    aaa = open(Path(__file__).parent / str("generados/" + apelidos + " " + nome + "/" + file), encoding="utf-8")
-                    print(aaa.name)
-                    #win32api.ShellExecute(0, 'print', aaa.name, f'/d:"{win32print.GetDefaultPrinter()}"', '.', 0)
-                else:
-                    print("fallo")
+    def prueba(self):
+        for row in self.sheet.data:
+            print(row)
 
 app = win()
 app.mainloop()
