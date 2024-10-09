441572a9cc5911ad85614600f706301a96a81a92
K4o-git <nachogb.op@gmail.com>
2024-10-09 13:23:29 +0200

    Terminada

--- app/app.py
+++ app/app.py
@@ -1,34 +1,27 @@
-import csv
 import os
 import sys
-import io
 from pathlib import Path
 
-import pandas as pd
 import tksheet
 from docxtpl import DocxTemplate
-import win32api
-import win32print
 import tkinter as tk
 from tkinter import *
-from tkinter import ttk, filedialog
+from tkinter import ttk, filedialog, messagebox
 from tksheet import Sheet
 from tkcalendar import DateEntry
 
 class win(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
-        self.title("Rellenar documentos para imprimir")
+        self.title("Cubrir documentos do curso e imprimir")
         self.resizable(False, False)
         self.geometry("1100x750")
-
+        
         barra_menus = Menu()
         menu = Menu(barra_menus, tearoff=False)
-        menu2 = Menu(barra_menus, tearoff=False)
         barra_menus.add_cascade(menu=menu, label="Arquivo")
-        barra_menus.add_cascade(menu=menu2, label="Opcions")
-        menu.add_command(label="Gardar datos alumnos", command=lambda: self.save_sheet())
-        menu.add_command(label="Cargar datos alumnos gardados", command=lambda: self.open_csv())
+        menu.add_command(label="Gardar datos alumnos", command=lambda: self.guardar_tabla())
+        menu.add_command(label="Cargar datos alumnos gardados", command=lambda: self.cargar_alumnos())
         menu.add_command(label="Guardar datos curso", command=lambda: self.gardar_datos_curso())
         menu.add_command(label="Cargar datos curso gardados", command=lambda: self.cargar_datos_curso())
         menu.add_command(label="Saír", command=lambda: sys.exit())
@@ -62,21 +55,15 @@ class win(tk.Tk):
                                      )
         self.centro.place(x=130, y=113)
         
-        # et_datas_curso = Label(self, text="Datas curso:")
-        # et_datas_curso.place(x=30, y=160)
-        # self.datas_curso = Entry(self, width=42,)
-        # self.datas_curso.place(x=130, y=160)
-        # self.datas_curso.insert(0, "23 de setembro de 2024 a 23 setembro de 2025")
-
         et_data_inicio = Label(self, text="Data inicio")
         et_data_inicio.place(x=30, y=144)
-        self.data_inicio = DateEntry(self, width=11)
+        self.data_inicio = DateEntry(self, locale='gl', width=11)
         self.data_inicio.place(x=130, y=144)
         self.data_inicio.config(date_pattern= "dd/mm/yyyy")
 
         et_data_finalizacion = Label(self, text="Data finalización")
         et_data_finalizacion.place(x=260, y=144)
-        self.data_finalizacion = DateEntry(self, width=11)
+        self.data_finalizacion = DateEntry(self, locale='gl', width=11)
         self.data_finalizacion.place(x=360, y=144)
         self.data_finalizacion.config(date_pattern= "dd/mm/yyyy")
 
@@ -94,8 +81,6 @@ class win(tk.Tk):
         self.documentos[3]= "Autorización_pegada_dixital.docx"
         self.documentos[4]= "Información_de_bolsas.docx"
         self.documentos[5]= "Autorización_datos_persoais.docx"
-        #self.documentos[6]= "MC_Modelo autorización datos persoais_2.docx"
-        #self.documentos[7]= "MC_Modelo autorización rexistro pegada dixital_gal.docx"
 
         self.checkbox_variable=[tk.IntVar(), tk.IntVar(), tk.IntVar(), tk.IntVar(), tk.IntVar(), tk.IntVar()]
 
@@ -106,8 +91,6 @@ class win(tk.Tk):
         self.checkbox4 = ttk.Checkbutton(self, text="Autorización pegada dixital", variable= self.checkbox_variable[3]).place(x=830, y=105)
         self.checkbox5 = ttk.Checkbutton(self, text="Información de bolsas", variable= self.checkbox_variable[4]).place(x=830, y=130)
         self.checkbox6 = ttk.Checkbutton(self, text="Autorización datos persoais", variable= self.checkbox_variable[5]).place(x=830, y=155)
-        #self.checkbox7 = ttk.Checkbutton(self, text="Modelo autorización datos persoais 2(Ames)", variable= self.checkbox_variable[6]).place(x=680, y=135)
-        #self.checkbox8 = ttk.Checkbutton(self, text="Mod. aut. rex. pegada dixital_gal(Ames)", variable= self.checkbox_variable[7]).place(x=680, y=155)
 
         self.checkbox_variable[0].set(0)
         self.checkbox_variable[1].set(0)
@@ -115,8 +98,6 @@ class win(tk.Tk):
         self.checkbox_variable[3].set(0)
         self.checkbox_variable[4].set(0)
         self.checkbox_variable[5].set(0)
-        #self.checkbox_variable[6].set(0)
-        #self.checkbox_variable[7].set(0)
 
         self.bot_marcar_todos_formularios = ttk.Button(self, text="Seleccionar todos", command= self.marcar_todos_documentos).place(x=800, y=185)
         self.bot_desmarcar_todos_formularios = ttk.Button(self, text="Deseleccionar todos", command= self.desmarcar_todos_documentos).place(x=925, y=185)
@@ -126,15 +107,15 @@ class win(tk.Tk):
 
         self.sheet = tksheet.Sheet(self, width=1100, height=450, total_columns=4, total_rows=15, show_x_scrollbar=False, show_y_scrollbar=True)
         self.sheet.column_width(column=0, width=140)
-        self.sheet.column_width(column=1, width=440)
-        self.sheet.column_width(column=2, width=440)
+        self.sheet.column_width(column=1, width=420)
+        self.sheet.column_width(column=2, width=460)
         self.sheet.column_width(column=3, width=24)
-        self.sheet.headers(["DNI", "Nome", "Apelidos", ""])
+        self.sheet.headers(["DNI / NIF / NIE", "Nome", "Apelidos", "✅"])
         self.sheet.checkbox("D", checked=False)
         self.sheet.enable_bindings()
         self.sheet.disable_bindings("column_width_resize", "rc_insert_column", "rc_delete_column")
-        self.sheet.popup_menu_add_command("Cargar archivo csv", self.open_csv)
-        self.sheet.popup_menu_add_command("Gardar datos a CSV", self.save_sheet)
+        self.sheet.popup_menu_add_command("Cargar archivo csv", self.cargar_alumnos)
+        self.sheet.popup_menu_add_command("Gardar datos a CSV", self.guardar_tabla)
         self.sheet.place(x=0, y=250)
 
         self.sheet_span = self.sheet.span(
@@ -144,11 +125,15 @@ class win(tk.Tk):
             idisp=False,
         )
 
-        self.bot_marcar_todos_alum = ttk.Button(self, text="Seleccionar todos", command= self.marcar_todos_alumnos).place(x=725, y=712)
-        self.bot_desmarcar_todos_alum = ttk.Button(self, text="Deseleccionar todos", command= self.desmarcar_todos_alumnos).place(x=845, y=712)
+        self.valeirar_taboa = ttk.Button(self, width=17, text="Baleirar táboa", command=lambda: self.valeirar_listado_de_alumnos())
+        self.valeirar_taboa.place(x=540, y= 708, width=160, height=35)
+        self.bot_marcar_todos_alum = ttk.Button(self, width=17, text="Seleccionar todos", command= self.marcar_todos_alumnos)
+        self.bot_marcar_todos_alum.place(x=705, y=708, width=160, height=35)
+        self.bot_desmarcar_todos_alum = ttk.Button(self, width=17, text="Deseleccionar todos", command= self.desmarcar_todos_alumnos)
+        self.bot_desmarcar_todos_alum.place(x=870, y=708, width=160, height=35)
 
-        self.valeirar_taboa = ttk.Button(self, width=20, text="Baleirar táboa", command=lambda: self.valeirar_listado_de_alumnos()).place(x=120, y= 712)
-        self.botonOK = ttk.Button(self, width=20, text="Cubrir Documentos", command=lambda: self.prueba_generar_documentos()).place(x=320, y= 712)
+        self.botonOK = ttk.Button(self, text="IMPRIMIR DOCUMENTOS", command=lambda: self.generar_documentos())
+        self.botonOK.place(x=70, y= 708, width=160, height=35)
         
 
     def marcar_todos_documentos(self):
@@ -158,11 +143,6 @@ class win(tk.Tk):
         self.checkbox_variable[3].set(1)
         self.checkbox_variable[4].set(1)
         self.checkbox_variable[5].set(1)
-        #self.checkbox_variable[6].set(1)
-        #self.checkbox_variable[7].set(1)
-        print(self.data_inicio.get_date().year)
-        print(self.meses[self.data_inicio.get_date().month-1])
-        print(self.data_inicio.get_date().day)
 
     def desmarcar_todos_documentos(self):
         self.checkbox_variable[0].set(0)
@@ -171,17 +151,13 @@ class win(tk.Tk):
         self.checkbox_variable[3].set(0)
         self.checkbox_variable[4].set(0)
         self.checkbox_variable[5].set(0)
-        #self.checkbox_variable[6].set(0)
-        #self.checkbox_variable[7].set(0)
 
-    
     def marcar_todos_alumnos(self):
         n=1
         for row in self.sheet.data:
             if self.sheet["A"+str(n)].data:
                 self.sheet["D"+str(n)].data=True
             n += 1
-            print(n)
 
     def desmarcar_todos_alumnos(self):
         n=1
@@ -195,10 +171,10 @@ class win(tk.Tk):
             self.sheet["A"+str(n)].data=["", "", ""]
             n += 1
         
-    def save_sheet(self):
+    def guardar_tabla(self):
         filepath = filedialog.asksaveasfilename(
             parent=self,
-            title="Save sheet as",
+            title="Gardar listado de alumnos",
             filetypes=[("CSV File", ".csv"), ("TSV File", ".tsv")],
             defaultextension=".csv",
             confirmoverwrite=True
@@ -207,37 +183,36 @@ class win(tk.Tk):
             return
         try:
             file = open(filepath, 'w+')
-            ##file.write("DNI;Nome;Apelidos;ok\n")
-            #file.write(self.num_curso.get() + ";" + self.nom_curso.get() + ";" + "\n")
             for row in self.sheet.data:
-                check = ''
-                if (row[3]): check = str(row[3])
-                file.write(row[0] + ";" + row[1] + ";" + row[2] + ";" + check + "\n")
+                file.write(row[0] + ";" + row[1] + ";" + row[2] + ";\n")
         except FileNotFoundError:
             print('ERROR')
 
-    def open_csv(self):
-        filepath = filedialog.askopenfilename(parent=self, title="Cargar archivo CSV")
+    def cargar_alumnos(self):
+        filepath = filedialog.askopenfilename(parent=self, title="Cargar litado de alumnos")
         if not filepath or not filepath.lower().endswith((".csv", ".tsv")):
             return
         try:
             with open(Path(filepath), "r") as file:
                 self.sheet.data.clear()
                 i = 1
-                for linea in file: 
+                for linea in file:
+                    if not (linea.split(';').__len__() == 4):
+                        messagebox.showerror("Erro", "Arquivo non válido")
+                        break
                     dni = linea.split(';')[0]
                     nome = linea.split(';')[1]
                     apelidos = linea.split(';')[2]
                     self.sheet["A" + str(i)].data = [dni, nome, apelidos, False]
                     i += 1
         except Exception as error:
-            print(error)
+            messagebox.showerror("Erro", "Arquivo non válido")
             return
         
     def gardar_datos_curso(self):
         filepath = filedialog.asksaveasfilename(
             parent=self,
-            title="Save sheet as",
+            title="Gardar datos do curso",
             filetypes=[("CSV File", ".csv"), ("TSV File", ".tsv")],
             defaultextension=".csv",
             confirmoverwrite=True
@@ -250,16 +225,15 @@ class win(tk.Tk):
                        + self.cod_espec.get() + ";" 
                        + self.nom_curso.get() + ";"
                        + self.centro.get() + ";" 
-                       #+ self.datas_curso.get() + ";"
                        + self.data_inicio.get() + ";"
                        + self.data_finalizacion.get() + ";"
-                       + self.num_censo.get() + ";"  
+                       + self.num_censo.get()
                        )
         except FileNotFoundError:
             print('ERROR')
 
     def cargar_datos_curso(self):
-        filepath = filedialog.askopenfilename(parent=self, title="Cargar archivo CSV")
+        filepath = filedialog.askopenfilename(parent=self, title="Cargar archivo datos do curso")
         if not filepath or not filepath.lower().endswith((".csv", ".tsv")):
             return
         try:
@@ -268,24 +242,25 @@ class win(tk.Tk):
                 self.cod_espec.delete(0, tk.END) 
                 self.nom_curso.delete(0, tk.END)
                 self.centro.delete(0, tk.END) 
-                #self.datas_curso.delete(0, tk.END)
-                self.num_censo.delete(0, tk.END) 
-                for linea in file: 
+                self.num_censo.delete(0, tk.END)
+                for linea in file:
+                    if not (linea.split(';').__len__() == 7):
+                        messagebox.showerror("Erro", "Arquivo non válido")
+                        break
                     self.cod_ac_form.insert(0, linea.split(';')[0]) 
                     self.cod_espec.insert(0, linea.split(';')[1])
                     self.nom_curso.insert(0, linea.split(';')[2])
                     self.centro.insert(0, linea.split(';')[3])
-                    #self.datas_curso.insert(0, linea.split(';')[4])
-                    self.data_inicio.insert(0, linea.split(';')[4])
-                    self.data_finalizacion.insert(0, linea.split(';')[5])
+                    self.data_inicio.set_date(linea.split(';')[4])
+                    self.data_finalizacion.set_date(linea.split(';')[5])
                     self.num_censo.insert(0, linea.split(';')[6])
-                
         except Exception as error:
             print(error)
             return   
         
-    def prueba_generar_documentos(self):
+    def generar_documentos(self):
         for row in self.sheet.data:
+            impresion = []
             try:
                 if (row[0] == "") or (row[3] == None): break
                 if row[3]:
@@ -296,7 +271,6 @@ class win(tk.Tk):
                     cod_espec = self.cod_espec.get()
                     nome_curso = self.nom_curso.get()
                     centro = self.centro.get()
-                    #datas_curso = self.datas_curso.get()
                     dia_inicio = self.data_inicio.get_date().day
                     mes_inicio = self.meses[self.data_inicio.get_date().month-1]
                     ano_inicio = self.data_inicio.get_date().year
@@ -313,7 +287,6 @@ class win(tk.Tk):
                         "COD_ESPEC": cod_espec,
                         "NOME_CURSO": nome_curso,
                         "CENTRO": centro,
-                        #"DATAS_CURSO": datas_curso,
                         "DIA_INICIO": dia_inicio,
                         "MES_INICIO": mes_inicio,
                         "ANO_INICIO": ano_inicio,
@@ -322,26 +295,21 @@ class win(tk.Tk):
                         "ANO_FINALIZACION": ano_finalizacion,
                         "NUM_CENSO": num_censo,
                     }
-                    # Crea las subcarpetas para cada alumno y cuarda dentro sus documentos modificados
-                    os.makedirs(str("xerados/" + apelidos + " " + nome))
-                    for file in os.listdir("modelos"):
+                    
+                    os.makedirs(str("xerados/" + dni), exist_ok=True)
+                    for file in os.listdir(os.path.join(Path(__file__).parent.parent.parent, "modelos")):
                         for n in range(6):
                             if str(file) == self.documentos[n] and self.checkbox_variable[n].get():
                                 if file.endswith("docx"):
-                                    documento_path = Path(__file__).parent / str("modelos/" + file)
+                                    documento_path = Path(__file__).parent.parent.parent / str("modelos/" + file)
                                     doc = DocxTemplate(documento_path)
                                     doc.render(context)
-                                    doc.save(Path(__file__).parent / str("xerados/" + apelidos + " " + nome + "/" + file))
-                                    aaa = open(Path(__file__).parent / str("xerados/" + apelidos + " " + nome + "/" + file), encoding="utf-8")
-                                    print(aaa.name)
-                                    #win32api.ShellExecute(0, 'print', aaa.name, f'/d:"{win32print.GetDefaultPrinter()}"', '.', 0)
+                                    norm_path = os.path.normpath(os.path.join(Path(__file__).parent.parent.parent, "xerados/" + str(dni), file))
+                                    doc.save(norm_path)
+                                    os.startfile(norm_path, "print")
                                 else:
                                     print("fallo")
             except Exception as e:
-                print(e)
-
-app = win()
-app.mainloop()
-
-
-               
+                messagebox.showerror("Erro", "Houbo un erro inesperado")
+                return
+        messagebox.showinfo("Feito", "FORMULARIOS GARDADOS")
